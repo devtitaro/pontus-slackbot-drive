bbfae0c692ace2bec671fa88d9aa00a1a3dd33d5
Itunuoluwao <46650010+Itunuoluwao@users.noreply.github.com>
2019-09-29 05:04:22 +0100

    Add files via upload

---
+++ assets/logout.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Pontus Slackbot
+ * 
+ * Details: This file is part of the pontus slackbot file
+ * Author: titaro
+ *
+ */
+
+// Destroy user login session
+session_start();   
+session_destroy();
+
+// Redirect to index page after logout
+header("location: index.php");
+
+?>
