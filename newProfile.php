b08908edbd66c7f2afa4bac3e2cd35b981c7d8e1
Joshua <olajidejoshua4real@gmail.com>
2019-10-06 03:31:59 +0100

    Added 2 new files newProfile.php and newProfile.css

---
+++ newProfile.php
@@ -0,0 +1,83 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons">
+    <link rel="stylesgeet" href="https://rawgit.com/creativetimofficial/material-kit/master/assets/css/material-kit.css">
+    
+    <link rel="stylesheet" href="assets/newProfile.css">
+    
+</head>
+
+<body class="profile-page">
+    
+
+    
+    <div class="page-header header-filter" data-parallax="true" style="background-image:url('http://wallpapere.org/wp-content/uploads/2012/02/black-and-white-city-night.png');"></div>
+    <div class="main main-raised">
+		<div class="profile-content">
+            <div class="container">
+                <div class="row">
+	                <div class="col-md-6 ml-auto mr-auto">
+        	           <div class="profile">
+	                        <div class="avatar">
+	                            <img src="https://via.placeholder.com/150" alt="Joshua Olajide" class="img-raised rounded-circle img-fluid">
+	                        </div>
+	                        <div class="name">
+	                            <h3 class="title">Joshua Olajide</h3>
+								<h6>FrontEnd Dev</h6>
+								<a href="#" class="btn btn-just-icon btn-link btn-dribbble"><i class="fa fa-linkedin"></i></a>
+                                <a href="#" class="btn btn-just-icon btn-link btn-twitter"><i class="fa fa-twitter"></i></a>
+                                <a href="#" class="btn btn-just-icon btn-link btn-pinterest"><i class="fa fa-github"></i></a>
+	                        </div>
+	                    </div>
+    	            </div>
+                </div>
+                <div class="description text-center">
+                    <p>I a Frontend engineer and i am more passionate about problem solving </p>
+                </div>
+				<div class="row">
+					<div class="col-md-6 ml-auto mr-auto">
+                        <div class="profile-tabs">
+                          <ul class="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
+                           
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#favorite" role="tab" data-toggle="tab">
+                                  <i class="material-icons">favorite</i>
+                                    Favorite Mesages
+                                </a>
+                            </li>
+                          </ul>
+                        </div>
+    	    	</div>
+            </div>
+        
+          <div class="tab-content ">
+           
+            <div class="text-center" id="favorite">
+      			<div class="row">
+      				<div class="col-md-12 ml-auto mr-auto ">
+      				 <h3 class="mt-4 mb-5">All favourite messages goes here</h3>
+      			    </div>
+                      
+                      
+      			</div>
+      		</div>
+          </div>
+
+        
+            </div>
+        </div>
+	</div>
+	
+
+    
+    <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+
+</body>
